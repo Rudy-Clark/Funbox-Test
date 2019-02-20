10e46b9e6f7505356529497948cfe6c5ceec30ee
Rudy Clark <rudy.uzb@gmail.com>
2019-02-20 10:22:31 +0500

    created App component => RouteWithInput, Map

---
+++ app/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import Map from '../containers/Map';
+import InputWithRoute from './InputWithRoute';
+
+const Container = styled.div`
+  max-width: 980px;
+  margin: 12px auto;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const App = () => (
+  <Container>
+    <InputWithRoute />
+    <Map />
+  </Container>
+);
+
+export default App;
