d3e672479b7cc4bf0792928395f02802b65c4f71
Rudy Clark <rudy.uzb@gmail.com>
2019-02-21 12:24:10 +0500

    import addRoute actions to container Form

--- app/containers/Form.js
+++ app/containers/Form.js
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import FormGroup from '../components/FormGroup';
+import { addRoute } from '../actions';
 
 const mapStateToProps = state => ({
   routes: state.routes,
 });
 
 const mapDispatchToProps = dispatch => ({
-  addRoute: id => dispatch(),
+  addRoute: id => dispatch(addRoute(id)),
 });
 
 export default connect(
