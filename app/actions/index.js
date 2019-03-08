8f6fcd5cfc6f387d65136ba49286966c7b6ab28c
Rudy Clark <rudy.uzb@gmail.com>
2019-03-08 13:53:22 +0500

    create action moveRoute && add dragTarget structure

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,6 +1,7 @@
 export const ADD_ROUTE = 'ADD_ROUTE';
 export const DELETE_ROUTE = 'DELETE_ROUTE';
 export const UPDATE_ROUTE = 'UPDATE_ROUTE';
+export const MOVE_ROUTE = 'MOVE_ROUTE';
 export const ROUTE = 'ROUTE';
 
 export const addRoute = routeName => ({
@@ -18,6 +19,12 @@ export const updateRoute = (id, changedName) => ({
   id,
   changedName,
   type: UPDATE_ROUTE,
+});
+
+export const moveRoute = (from, to) => ({
+  from,
+  to,
+  type: MOVE_ROUTE
 });
 
 // Generate unique id;
--- app/components/Route.js
+++ app/components/Route.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { DragSource } from 'react-dnd';
+import { DragSource, DropTarget } from 'react-dnd';
 import { ROUTE } from '../actions';
 
 const RouteCont = styled.div`
   margin: 0 0 7px;
-  display: ${props => props.isDragging ? 'none' : 'flex'};
+  display: ${props => (props.isDragging ? 'none' : 'flex')};
   opacity: 1;
   flex-flow: row nowrap;
   align-items: start;
   cursor: pointer;
+`;
+
+const RoutePlaceholder = styled.div`
+  display: block;
+  margin: 0 0 9px;
+  opacity: 1;
+  width: 100%;
+  height: 20px;
+  border: 2px solid #ffff66;
 `;
 
 const IconDelete = styled.svg`
@@ -34,33 +43,57 @@ const Title = styled.p`
   overflow: hidden;
 `;
 
-const Route = ({ routeName, deleteRoute, isDragging, connectDragSource }) =>
+const Route = ({
+  routeName,
+  deleteRoute,
+  isDragging,
+  connectDragSource,
+  connectDropTarget,
+  isOver
+}) =>
   connectDragSource(
-    <li>
-      <RouteCont isDragging={isDragging}>
-        <Title>{routeName}</Title>
-        <IconDelete onClick={deleteRoute} viewBox="0 0 14.098 14.098">
-          <path
-            style={{ fill: 'rgb(237, 70, 82)' }}
-            d="M13.198,0H0.901C0.404,0,0,0.403,0,0.9v12.299c0,0.496,0.404,0.9,0.901,0.9h12.298
-			    c0.497,0,0.9-0.404,0.9-0.9V0.901C14.098,0.403,13.694,0,13.198,0z M11.209,9.601l-1.608,1.607c-0.078,0.078-0.202,0.078-0.28,0
-			    L7.049,8.936l-2.272,2.273c-0.078,0.077-0.202,0.077-0.28,0L2.889,9.601c-0.077-0.078-0.077-0.202,0-0.28L5.16,7.049L2.888,4.777
-			    c-0.077-0.078-0.077-0.202,0-0.28l1.608-1.608c0.078-0.078,0.202-0.078,0.28,0l2.272,2.272L9.32,2.889
-			    c0.078-0.078,0.202-0.078,0.28,0l1.607,1.608c0.078,0.078,0.078,0.202,0,0.28l-2.27,2.272l2.272,2.272
-			    C11.286,9.399,11.286,9.523,11.209,9.601z"
-          />
-        </IconDelete>
-      </RouteCont>
-    </li>
+    connectDropTarget(
+      <li>
+        <RouteCont isDragging={isDragging}>
+          <Title>{routeName}</Title>
+          <IconDelete onClick={deleteRoute} viewBox="0 0 14.098 14.098">
+            <path
+              style={{ fill: 'rgb(237, 70, 82)' }}
+              d="M13.198,0H0.901C0.404,0,0,0.403,0,0.9v12.299c0,0.496,0.404,0.9,0.901,0.9h12.298
+			      c0.497,0,0.9-0.404,0.9-0.9V0.901C14.098,0.403,13.694,0,13.198,0z M11.209,9.601l-1.608,1.607c-0.078,0.078-0.202,0.078-0.28,0
+			      L7.049,8.936l-2.272,2.273c-0.078,0.077-0.202,0.077-0.28,0L2.889,9.601c-0.077-0.078-0.077-0.202,0-0.28L5.16,7.049L2.888,4.777
+			      c-0.077-0.078-0.077-0.202,0-0.28l1.608-1.608c0.078-0.078,0.202-0.078,0.28,0l2.272,2.272L9.32,2.889
+			      c0.078-0.078,0.202-0.078,0.28,0l1.607,1.608c0.078,0.078,0.078,0.202,0,0.28l-2.27,2.272l2.272,2.272
+			      C11.286,9.399,11.286,9.523,11.209,9.601z"
+            />
+          </IconDelete>
+        </RouteCont>
+        {isDragging && <RoutePlaceholder />}
+      </li>
+    )
   );
-
 const routeSource = {
-  beginDrag() {
-    return {};
+  beginDrag(props) {
+    return { id: props.id };
   }
 };
 
-const collect = (connect, monitor) => ({
+const routeTarget = {
+  drop(props) {
+    return {};
+  },
+  hover(props, monitor) {
+    const dragId = monitor.getItem().id;
+    console.log(dragId);
+  }
+};
+
+const collectTarget = (connect, monitor) => ({
+  connectDropTarget: connect.dropTarget(),
+  isOver: monitor.isOver()
+});
+
+const collectSource = (connect, monitor) => ({
   connectDragSource: connect.dragSource(),
   isDragging: monitor.isDragging()
 });
@@ -70,4 +103,6 @@ Route.propTypes = {
   deleteRoute: PropTypes.func.isRequired,
 };
 
-export default DragSource(ROUTE, routeSource, collect)(Route);
+export default DropTarget(ROUTE, routeTarget, collectTarget)(
+  DragSource(ROUTE, routeSource, collectSource)(Route)
+);
