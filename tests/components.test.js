dc0b5bb84f1f0797cfed911b1386cf8139f3abac
Rudy Clark <rudy.uzb@gmail.com>
2019-03-19 17:28:24 +0500

    add updated components tests

--- tests/components.test.js
+++ tests/components.test.js
@@ -1,69 +1,61 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import Enzyme, { mount, shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
+import '@babel/polyfill';
+import { mount, shallow } from 'enzyme';
 import 'jest-styled-components';
-import Adapter from 'enzyme-adapter-react-16';
-
 import Route from '../app/components/Route';
 import Map from '../app/components/Map';
 import App from '../app/components/App';
 import InputWithRoute from '../app/components/InputWithRoute';
-
-Enzyme.configure({ adapter: new Adapter() });
+import ListRoutes from '../app/components/ListRoutes';
+import DragRoute from '../app/containers/DragRoute';
+import FormGroup from '../app/components/FormGroup';
 
 describe('App', () => {
   it('App component snapshot', () => {
     const app = shallow(<App />);
-    expect(app).toMatchSnapshot();
     expect(app.exists()).toBe(true);
+    expect(app).toMatchSnapshot();
   });
 });
 
 describe('InputWithRoute', () => {
   it('InputWithRoute snapshot', () => {
     const inputWithRoute = shallow(<InputWithRoute />);
-    expect(inputWithRoute).toMatchSnapshot();
     expect(inputWithRoute.exists()).toBe(true);
+    expect(inputWithRoute).toMatchSnapshot();
   });
 });
 
 describe('Route', () => {
-  it('Route component snapshot', () => {
-    const route = shallow(<Route />);
-    expect(route).toMatchSnapshot();
-  });
-
+  const mock = {
+    routeName: 'test',
+    deleteRoute: jest.fn(),
+    isDragging: false,
+  };
+  const route = mount(
+    <Route
+      routeName={mock.routeName}
+      deleteRoute={mock.deleteRoute}
+      isDragging={mock.isDragging}
+    />,
+  );
+  const routerShallowRender = shallow(
+    <Route
+      routeName={mock.routeName}
+      deleteRoute={mock.deleteRoute}
+      isDragging={mock.isDragging}
+    />,
+  );
   it('Route component snapshot with props', () => {
-    const mock = {
-      routeName: 'test',
-      deleteRoute: jest.fn(),
-      isDragging: false,
-    };
-
-    const route = shallow(
-      <Route
-        routeName={mock.routeName}
-        deleteRoute={mock.deleteRoute}
-        isDragging={mock.isDragging}
-      />,
-    );
-    expect(route).toMatchSnapshot();
-  });
-
-  it('check Route render', () => {
-    const route = shallow(<Route />);
-    expect(route.exists()).toBe(true);
+    expect(routerShallowRender).toMatchSnapshot();
   });
 
   it('check routeName text', () => {
-    const routeName = 'Test route';
-    const route = mount(<Route routeName={routeName} />);
-    expect(route.text()).toEqual(routeName);
+    expect(route.text()).toEqual(mock.routeName);
   });
 
   it('check Route elements', () => {
-    const route = mount(<Route />);
     const cont = route.find('div');
     expect(cont.length).toBe(2);
     expect(cont.find('div div').length).toBe(1);
@@ -72,20 +64,79 @@ describe('Route', () => {
   });
 
   it('check delete route', () => {
-    const spyClick = jest.fn();
-    const route = mount(<Route deleteRoute={spyClick} />);
     route.find('svg').simulate('click');
-    expect(spyClick).toHaveBeenCalled();
+    expect(mock.deleteRoute).toHaveBeenCalled();
+  });
+
+  it('Route contains props', () => {
+    expect(route).toHaveProp({
+      routeName: mock.routeName,
+      isDragging: mock.isDragging,
+      deleteRoute: mock.deleteRoute,
+    });
+  });
+});
+
+describe('ListRoutes', () => {
+  const mock = {
+    deleteRoute: id => jest.fn(id),
+    moveRoute: jest.fn(),
+  };
+
+  const routes = [{ id: 'pop', routeName: 'lorem ipsum' }];
+  const listRoutesFull = mount(
+    <ListRoutes
+      deleteRoute={() => mock.deleteRoute('pop')}
+      moveRoute={mock.moveRoute}
+      routes={routes}
+    />,
+  );
+
+  const listRoutes = shallow(
+    <ListRoutes
+      deleteRoute={mock.deleteRoute}
+      moveRoute={mock.moveRoute}
+      routes={[]}
+    />,
+  );
+
+  it('ListRoutes component snapshot with props', () => {
+    expect(listRoutes.exists()).toBe(true);
+    expect(listRoutes).toMatchSnapshot();
+  });
+
+  it('check contains Routes', () => {
+    expect(listRoutesFull.find(DragRoute).length).toBe(1);
+  });
+});
+
+describe('FormGroup', () => {
+  const mock = {
+    addRoute: jest.fn(),
+  };
+  const formGroup = shallow(<FormGroup addRoute={mock.addRoute} />);
+  const formGroupFull = mount(<FormGroup addRoute={mock.addRoute} />);
+
+  it('FormGroup snapshot with props', () => {
+    expect(formGroup.exists()).toBe(true);
+    expect(formGroup).toMatchSnapshot();
+  });
+
+  it('FormGroup check addRoute', () => {
+    const value = 'test route';
+    const form = formGroupFull.find('form');
+    form.find('input').instance().value = value;
+    form.simulate('submit');
+    expect(mock.addRoute).toHaveBeenCalled();
+    expect(mock.addRoute.mock.calls.length).toBe(1);
+    expect(mock.addRoute.mock.calls[0]).toBe(value);
   });
 });
 
 describe('Map', () => {
-  it('Map component snapshot', () => {
-    const map = mount(<Map />);
-    expect(map).toMatchSnapshot();
-  });
-  it('Map exists', () => {
+  it('Map snapshots', () => {
     const map = shallow(<Map />);
     expect(map.exists()).toBe(true);
+    expect(map).toMatchSnapshot();
   });
 });
