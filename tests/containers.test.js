89658896952bf16f0e96ac3c63c31985801e4af2
Rudy Clark <rudy.uzb@gmail.com>
2019-03-21 13:40:53 +0500

    add heroku run script && RouteList test

--- tests/containers.test.js
+++ tests/containers.test.js
@@ -3,15 +3,17 @@ import React from 'react';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import { mount } from 'enzyme';
+import { combineReducers } from 'redux';
 
 import Form from '../app/containers/Form';
+import RouteList from '../app/containers/RouteList';
 
-describe('FormCont', () => {
+describe('Form', () => {
   let initialState = {};
   let store;
   // eslint-disable-next-line no-unused-vars
   let form;
-  const mockStore = configureStore();
+  const mockStore = configureStore([]);
 
   beforeEach(() => {
     store = mockStore(initialState);
@@ -28,5 +30,44 @@ describe('FormCont', () => {
     store.dispatch(addRoute(value));
     const actions = store.getActions();
     expect(actions).toEqual([{ type: 'ADD', routeName: value }]);
+    store.dispatch(addRoute('second'));
+    expect(actions.length).toBe(2);
+    expect(actions).toContainEqual({ type: 'ADD', routeName: 'second' });
+  });
+});
+
+describe('RouteList', () => {
+  let initialState = [
+    { id: 'pop', routeName: 'sweet dreams' },
+    { id: 'rap', routeName: 'candy shop' },
+  ];
+  let store;
+  // eslint-disable-next-line no-unused-vars
+  let list;
+  const mockStore = configureStore([]);
+
+  beforeEach(() => {
+    store = mockStore(
+      combineReducers({
+        routes: (state = initialState) => state,
+      }),
+    );
+    list = mount(
+      <Provider store={store}>
+        <RouteList />
+      </Provider>,
+    );
+  });
+
+  it('delete route dispatch', () => {
+    const deleteRoute = { type: 'DELETE' };
+    const actions = store.getActions();
+    store.dispatch(deleteRoute);
+    expect(actions).toContainEqual(deleteRoute);
+  });
+
+  it('check RouteList contain elements', () => {
+    const res = list.find('li');
+    expect(res.length).toBe(2);
   });
 });
